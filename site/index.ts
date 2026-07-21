382651fb6813aa321fbc167a620ce350260bad6c
dev-wooyeon <une@kakao.com>
2026-07-21 11:11:06 +0900

    refactor(site): 미사용 코드와 자산 정리
    도달하지 않는 블로그와 시리즈 UI, 전용 테스트, barrel export를 제거했습니다.
    
    미참조 정적 파일과 사용하지 않는 의존성도 함께 정리했습니다.

--- site/index.ts
+++
@@ -1,8 +0,0 @@
-export { AppShell } from './shell/AppShell';
-export { default as AppProviders } from './providers/AppProviders';
-export {
-  SITE_AUTHOR,
-  SITE_DESCRIPTION,
-  SITE_NAME,
-  SITE_URL,
-} from './config/site';
